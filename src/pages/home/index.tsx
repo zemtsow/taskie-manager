28b9864776102a605462ef27d6b8d872d801b902
zemtsow <nickwebtm@gmail.com>
2025-03-23 23:06:14 +0500

    own styles added

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,11 +1,13 @@
 import AddTaskInput from "@/components/AddTaskInput"
 import TaskList from "@/components/TaskList"
-import { Link } from "react-router-dom"
+import logo from '@/assets/logo.svg'
 
 const Home: React.FC = () => {
   return (
     <div className="mobileS:w-[90%] lg:w-[40%] mx-auto my-10">
-      <h1 className="text-blue-500 font-bold text-2xl text-center">Todo App by <Link to={"https://github.com/zemtsow"} target="_blank" className="underline">zemtsow</Link></h1>
+      <div className="">
+        <img src={logo} className="w-[200px] select-none" />
+      </div>
       <AddTaskInput />
       <TaskList />
     </div>
