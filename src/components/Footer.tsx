94e2094a54266e7020239f1e35b22c809f688493
zemtsow <nickwebtm@gmail.com>
2025-03-24 16:11:04 +0500

    footer fixed position removed

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import packageJson from '../../package.json';
 
 const Footer = () => {
     return (
-        <footer className='fixed bottom-0 left-0 right-0 w-full p-6 text-center flex mobileS:flex-col lg:flex-row justify-center items-center gap-2'>
+        <footer className=' w-full p-6 text-center flex mobileS:flex-col lg:flex-row justify-center items-center gap-2'>
             <p className='text-secondary opacity-50'>© Taskie - all rights reserved</p>
             <div className='text-secondary opacity-50 select-none pointer-events-none mobileS:hidden lg:block'>|</div>
             <Link to={"https://github.com/zemtsow/taskie-manager/releases/latest"} target='_blank' className='text-secondary select-none'>{packageJson.version}</Link>
